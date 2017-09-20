dc105491d3af958d7666c2b140adf2d28232ab71
werliton <werlitoncarlos@gmail.com>
2017-09-20 16:23:26 -0300

    Modularizando template

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,46 +1,42 @@
-</div>
-<!-- /page content -->
-
- <!-- footer content -->
-        <footer>
-          <div class="pull-right">
-            Gentelella - Bootstrap Admin Template by <a href="https://colorlib.com">Colorlib</a>
-          </div>
-          <div class="clearfix"></div>
-        </footer>
-        <!-- /footer content -->
-      </div>
+<footer>
+    <div class="pull-right">
+        Gentelella - Bootstrap Admin Template by <a href="https://colorlib.com">Colorlib</a>
     </div>
+    <div class="clearfix"></div>
+</footer>
+<!-- /footer content -->
+</div>
+</div>
 
-    <!-- jQuery -->
-    <script src="../vendors/jquery/dist/jquery.min.js"></script>
-    <!-- Bootstrap -->
-    <script src="../vendors/bootstrap/dist/js/bootstrap.min.js"></script>
-    <!-- FastClick -->
-    <script src="../vendors/fastclick/lib/fastclick.js"></script>
-    <!-- NProgress -->
-    <script src="../vendors/nprogress/nprogress.js"></script>
-    <!-- Chart.js -->
-    <script src="../vendors/Chart.js/dist/Chart.min.js"></script>
-    <!-- jQuery Sparklines -->
-    <script src="../vendors/jquery-sparkline/dist/jquery.sparkline.min.js"></script>
-    <!-- Flot -->
-    <script src="../vendors/Flot/jquery.flot.js"></script>
-    <script src="../vendors/Flot/jquery.flot.pie.js"></script>
-    <script src="../vendors/Flot/jquery.flot.time.js"></script>
-    <script src="../vendors/Flot/jquery.flot.stack.js"></script>
-    <script src="../vendors/Flot/jquery.flot.resize.js"></script>
-    <!-- Flot plugins -->
-    <script src="../vendors/flot.orderbars/js/jquery.flot.orderBars.js"></script>
-    <script src="../vendors/flot-spline/js/jquery.flot.spline.min.js"></script>
-    <script src="../vendors/flot.curvedlines/curvedLines.js"></script>
-    <!-- DateJS -->
-    <script src="../vendors/DateJS/build/date.js"></script>
-    <!-- bootstrap-daterangepicker -->
-    <script src="../vendors/moment/min/moment.min.js"></script>
-    <script src="../vendors/bootstrap-daterangepicker/daterangepicker.js"></script>
-    
-    <!-- Custom Theme Scripts -->
-    <script src="../build/js/custom.min.js"></script>
-  </body>
+<!-- jQuery -->
+<script src="/css/theme-gentelela/jquery/dist/jquery.min.js"></script>
+<!-- Bootstrap -->
+<script src="/css/theme-gentelela/bootstrap/dist/js/bootstrap.min.js"></script>
+<!-- FastClick -->
+<script src="/css/theme-gentelela/fastclick/lib/fastclick.js"></script>
+<!-- NProgress -->
+<script src="/css/theme-gentelela/nprogress/nprogress.js"></script>
+<!-- Chart.js -->
+<script src="/css/theme-gentelela/Chart.js/dist/Chart.min.js"></script>
+<!-- jQuery Sparklines -->
+<script src="/css/theme-gentelela/jquery-sparkline/dist/jquery.sparkline.min.js"></script>
+<!-- Flot -->
+<script src="/css/theme-gentelela/Flot/jquery.flot.js"></script>
+<script src="/css/theme-gentelela/Flot/jquery.flot.pie.js"></script>
+<script src="/css/theme-gentelela/Flot/jquery.flot.time.js"></script>
+<script src="/css/theme-gentelela/Flot/jquery.flot.stack.js"></script>
+<script src="/css/theme-gentelela/Flot/jquery.flot.resize.js"></script>
+<!-- Flot plugins -->
+<script src="/css/theme-gentelela/flot.orderbars/js/jquery.flot.orderBars.js"></script>
+<script src="/css/theme-gentelela/flot-spline/js/jquery.flot.spline.min.js"></script>
+<script src="/css/theme-gentelela/flot.curvedlines/curvedLines.js"></script>
+<!-- DateJS -->
+<script src="/css/theme-gentelela/DateJS/build/date.js"></script>
+<!-- bootstrap-daterangepicker -->
+<script src="/css/theme-gentelela/moment/min/moment.min.js"></script>
+<script src="/css/theme-gentelela/bootstrap-daterangepicker/daterangepicker.js"></script>
+
+<!-- Custom Theme Scripts -->
+<script src="/css/build/js/custom.min.js"></script>
+</body>
 </html>
--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -1,31 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <!-- Meta, title, CSS, favicons, etc. -->
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>Caúra Materiais de Construção </title>
-
-    <!-- Bootstrap -->
-    <link href="/css/theme-gentelela/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
-    <!-- Font Awesome -->
-    <link href="/css/theme-gentelela/font-awesome/css/font-awesome.min.css" rel="stylesheet">
-    <!-- NProgress -->
-    <link href="/css/theme-gentelela/nprogress/nprogress.css" rel="stylesheet">
-    <!-- bootstrap-daterangepicker -->
-    <link href="/css/theme-gentelela/bootstrap-daterangepicker/daterangepicker.css" rel="stylesheet">
-
-    <!-- Custom Theme Style -->
-    <link href="/css/build/css/custom.min.css" rel="stylesheet">
-  </head>
-
-  <body class="nav-md">
-    <div class="container body">
-      <div class="main_container">
-        <div class="col-md-3 left_col">
+@include('layout.header')
           <div class="left_col scroll-view">
             <div class="navbar nav_title" style="border: 0;">
               <a href="index.html" class="site_title"><i class="fa fa-paw"></i> <span>Caúra Materiais de Construção</span></a>
@@ -46,53 +19,8 @@
             <!-- /menu profile quick info -->
 
             <br />
-
             <!-- sidebar menu -->
-            <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
-              <div class="menu_section">
-                <h3>General</h3>
-                <ul class="nav side-menu">
-                  <li><a><i class="fa fa-home"></i> Home <span class="fa fa-chevron-down"></span></a>
-                    <ul class="nav child_menu">
-                      <li><a href="index.html">Dashboard</a></li>
-                      <li><a href="index2.html">Dashboard2</a></li>
-                      <li><a href="index3.html">Dashboard3</a></li>
-                    </ul>
-                  </li>
-				  
-				  <li><a><i class="fa fa-book"></i> Cadastro <span class="fa fa-chevron-down"></span></a>
-                    <ul class="nav child_menu">
-                      <li><a href="/produtos/novo">Produtos</a></li>
-                      <li><a href="/fornecedor/novo">Fornecedor</a></li>
-                    </ul>
-                  </li>
-				  
-                  <li><a><i class="fa fa-money"></i> Vendas <span class="fa fa-chevron-down"></span></a>
-                    <ul class="nav child_menu">
-                      <li><a href="/vendas/novo">Nova Venda</a></li>
-                      <li><a href="/vendas/realizadas">Realizadas</a></li>
-                      <li><a href="form_advanced.html">Canceladas</a></li>
-                    </ul>
-                  </li>
-                  <li><a><i class="fa fa-tasks"></i> Estoque <span class="fa fa-chevron-down"></span></a>
-                    <ul class="nav child_menu">
-                      <li><a href="general_elements.html">General Elements</a></li>
-                      <li><a href="media_gallery.html">Media Gallery</a></li>
-                      <li><a href="typography.html">Typography</a></li>
-                      <li><a href="icons.html">Icons</a></li>
-                      <li><a href="glyphicons.html">Glyphicons</a></li>
-                      <li><a href="widgets.html">Widgets</a></li>
-                      <li><a href="invoice.html">Invoice</a></li>
-                      <li><a href="inbox.html">Inbox</a></li>
-                      <li><a href="calendar.html">Calendar</a></li>
-                    </ul>
-                  </li>
-                  
-                                
-                 </ul>
-              </div>
-
-            </div>
+            @include('layout.menu')
             <!-- /sidebar menu -->
 
             <!-- /menu footer buttons -->
@@ -115,150 +43,14 @@
         </div>
 
         <!-- top navigation -->
-        <div class="top_nav">
-          <div class="nav_menu">
-            <nav>
-              <div class="nav toggle">
-                <a id="menu_toggle"><i class="fa fa-bars"></i></a>
-              </div>
-
-              <ul class="nav navbar-nav navbar-right">
-                <li class="">
-                  <a href="javascript:;" class="user-profile dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
-                    <img src="images/img.jpg" alt="">John Doe
-                    <span class=" fa fa-angle-down"></span>
-                  </a>
-                  <ul class="dropdown-menu dropdown-usermenu pull-right">
-                    <li><a href="javascript:;"> Profile</a></li>
-                    <li>
-                      <a href="javascript:;">
-                        <span class="badge bg-red pull-right">50%</span>
-                        <span>Settings</span>
-                      </a>
-                    </li>
-                    <li><a href="javascript:;">Help</a></li>
-                    <li><a href="login.html"><i class="fa fa-sign-out pull-right"></i> Log Out</a></li>
-                  </ul>
-                </li>
-
-                <li role="presentation" class="dropdown">
-                  <a href="javascript:;" class="dropdown-toggle info-number" data-toggle="dropdown" aria-expanded="false">
-                    <i class="fa fa-envelope-o"></i>
-                    <span class="badge bg-green">6</span>
-                  </a>
-                  <ul id="menu1" class="dropdown-menu list-unstyled msg_list" role="menu">
-                    <li>
-                      <a>
-                        <span class="image"><img src="images/img.jpg" alt="Profile Image" /></span>
-                        <span>
-                          <span>John Smith</span>
-                          <span class="time">3 mins ago</span>
-                        </span>
-                        <span class="message">
-                          Film festivals used to be do-or-die moments for movie makers. They were where...
-                        </span>
-                      </a>
-                    </li>
-                    <li>
-                      <a>
-                        <span class="image"><img src="images/img.jpg" alt="Profile Image" /></span>
-                        <span>
-                          <span>John Smith</span>
-                          <span class="time">3 mins ago</span>
-                        </span>
-                        <span class="message">
-                          Film festivals used to be do-or-die moments for movie makers. They were where...
-                        </span>
-                      </a>
-                    </li>
-                    <li>
-                      <a>
-                        <span class="image"><img src="images/img.jpg" alt="Profile Image" /></span>
-                        <span>
-                          <span>John Smith</span>
-                          <span class="time">3 mins ago</span>
-                        </span>
-                        <span class="message">
-                          Film festivals used to be do-or-die moments for movie makers. They were where...
-                        </span>
-                      </a>
-                    </li>
-                    <li>
-                      <a>
-                        <span class="image"><img src="images/img.jpg" alt="Profile Image" /></span>
-                        <span>
-                          <span>John Smith</span>
-                          <span class="time">3 mins ago</span>
-                        </span>
-                        <span class="message">
-                          Film festivals used to be do-or-die moments for movie makers. They were where...
-                        </span>
-                      </a>
-                    </li>
-                    <li>
-                      <div class="text-center">
-                        <a>
-                          <strong>See All Alerts</strong>
-                          <i class="fa fa-angle-right"></i>
-                        </a>
-                      </div>
-                    </li>
-                  </ul>
-                </li>
-              </ul>
-            </nav>
-          </div>
-        </div>
+        @include('layout.top-navegation')
         <!-- /top navigation -->
 
          <!-- page content -->
         <div class="right_col" role="main">
-
           @yield('content')
-
         </div>
         <!-- /page content -->
 
- <!-- footer content -->
-        <footer>
-          <div class="pull-right">
-            Gentelella - Bootstrap Admin Template by <a href="https://colorlib.com">Colorlib</a>
-          </div>
-          <div class="clearfix"></div>
-        </footer>
-        <!-- /footer content -->
-      </div>
-    </div>
-
-    <!-- jQuery -->
-    <script src="/css/theme-gentelela/jquery/dist/jquery.min.js"></script>
-    <!-- Bootstrap -->
-    <script src="/css/theme-gentelela/bootstrap/dist/js/bootstrap.min.js"></script>
-    <!-- FastClick -->
-    <script src="/css/theme-gentelela/fastclick/lib/fastclick.js"></script>
-    <!-- NProgress -->
-    <script src="/css/theme-gentelela/nprogress/nprogress.js"></script>
-    <!-- Chart.js -->
-    <script src="/css/theme-gentelela/Chart.js/dist/Chart.min.js"></script>
-    <!-- jQuery Sparklines -->
-    <script src="/css/theme-gentelela/jquery-sparkline/dist/jquery.sparkline.min.js"></script>
-    <!-- Flot -->
-    <script src="/css/theme-gentelela/Flot/jquery.flot.js"></script>
-    <script src="/css/theme-gentelela/Flot/jquery.flot.pie.js"></script>
-    <script src="/css/theme-gentelela/Flot/jquery.flot.time.js"></script>
-    <script src="/css/theme-gentelela/Flot/jquery.flot.stack.js"></script>
-    <script src="/css/theme-gentelela/Flot/jquery.flot.resize.js"></script>
-    <!-- Flot plugins -->
-    <script src="/css/theme-gentelela/flot.orderbars/js/jquery.flot.orderBars.js"></script>
-    <script src="/css/theme-gentelela/flot-spline/js/jquery.flot.spline.min.js"></script>
-    <script src="/css/theme-gentelela/flot.curvedlines/curvedLines.js"></script>
-    <!-- DateJS -->
-    <script src="/css/theme-gentelela/DateJS/build/date.js"></script>
-    <!-- bootstrap-daterangepicker -->
-    <script src="/css/theme-gentelela/moment/min/moment.min.js"></script>
-    <script src="/css/theme-gentelela/bootstrap-daterangepicker/daterangepicker.js"></script>
-    
-    <!-- Custom Theme Scripts -->
-    <script src="/css/build/js/custom.min.js"></script>
-  </body>
-</html>
+        <!-- footer content -->
+        @include('layout.footer')
